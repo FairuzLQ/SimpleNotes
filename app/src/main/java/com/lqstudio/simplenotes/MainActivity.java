4a6d729f640ef2bee776b2cfb96f9dde6e0ace04
FairuzLQ <elqimochammad12@gmail.com>
2023-02-23 13:51:24 +0700

    add recycler view data in main

--- app/src/main/java/com/lqstudio/simplenotes/MainActivity.java
+++ app/src/main/java/com/lqstudio/simplenotes/MainActivity.java
@@ -2,19 +2,23 @@ package com.lqstudio.simplenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton addNote;
     RecyclerView recyclerView;
     ImageButton menuHamburger;
+    NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void setupRecyclerView(){
+        Query query = Utility.ambilReferensiNote().orderBy("tanggal",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/lqstudio/simplenotes/NoteAdapter.java
+++ app/src/main/java/com/lqstudio/simplenotes/NoteAdapter.java
@@ -1,5 +1,49 @@
 package com.lqstudio.simplenotes;
 
-public class NoteAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteViewHolder> {
+    Context context;
+    public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull Note note) {
+        holder.judulView.setText(note.judul);
+        holder.deskripsiView.setText(note.deskripsi);
+        holder.waktuView.setText(note.tanggal);
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_note_item,parent,false);
+        return new NoteViewHolder(view);
+    }
+
+    class NoteViewHolder extends RecyclerView.ViewHolder {
+
+        TextView judulView, deskripsiView, waktuView;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            judulView = itemView.findViewById(R.id.recycler_judul);
+            deskripsiView = itemView.findViewById(R.id.recycler_deskripsi);
+            waktuView = itemView.findViewById(R.id.recycler_waktu);
+
+        }
+    }
 }
